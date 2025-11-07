1ea2c70641b0199bc8e9aacdd94f6acaf0249484
Agustin21986 <noelsosa1986@gmail.com>
2025-11-06 22:03:49 -0300

    actualizacion final

--- carrito.js
+++ carrito.js
@@ -1,10 +1,9 @@
-// --- VARIABLES ---
+
 let total = 0;
 const totalDisplay = document.getElementById("cart-total");
 const resetButton = document.getElementById("reset-cart");
 const addButtons = document.querySelectorAll(".add-to-cart");
 
-// --- AGREGAR PRODUCTO ---
 addButtons.forEach(button => {
     button.addEventListener("click", () => {
         const price = parseFloat(button.getAttribute("data-price"));
@@ -13,7 +12,6 @@ addButtons.forEach(button => {
     });
 });
 
-// --- REINICIAR CARRITO ---
 resetButton.addEventListener("click", () => {
     total = 0;
     totalDisplay.textContent = total.toFixed(2);
